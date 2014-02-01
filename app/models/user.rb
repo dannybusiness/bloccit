0baea61c0a3ca36b36d035b86f59b1c2331813f3
Daniel Mortensen <daniel.j.mortensen@gmail.com>
2014-02-01 17:32:44 +0000

    added Facebook authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook]
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar, :avatar_cache, :comments
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar, :avatar_cache, :comments, :provider, :uid
   
   has_many :posts
   has_many :comments
@@ -21,12 +16,27 @@ class User < ActiveRecord::Base
     role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
   end
 
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      pass = Devise.friendly_token[0,20]
+      user = User.new(name: auth.extra.raw_info.name,
+                         provider: auth.provider,
+                         uid: auth.uid,
+                         email: auth.info.email,
+                         password: pass,
+                         password_confirmation: pass
+                        )
+      user.skip_confirmation!
+      user.save
+    end
+    user
+  end
+
   private
 
     def set_member
       self.role = 'member'
     end
-
-  # validates :title, :length => { :minimum => 5 }
 
 end
